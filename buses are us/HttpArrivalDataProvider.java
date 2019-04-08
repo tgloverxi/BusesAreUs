6c9c5cbb0ae77de468e1a58cfdee6991ae673450
tgloverxi <37313482+tgloverxi@users.noreply.github.com>
2019-04-07 19:12:36 -0700

    Add files via upload

---
+++ buses are us/HttpArrivalDataProvider.java
@@ -0,0 +1,41 @@
+package ca.ubc.cs.cpsc210.translink.providers;
+
+import ca.ubc.cs.cpsc210.translink.model.Stop;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Wrapper for Translink Arrival Data Provider
+ */
+public class HttpArrivalDataProvider extends AbstractHttpDataProvider {
+    private Stop stop;
+
+    public HttpArrivalDataProvider(Stop stop) {
+        super();
+        this.stop = stop;
+    }
+
+    @Override
+    /**
+     * Produces URL used to query Translink web service for expected arrivals at
+     * the stop specified in call to constructor.
+     *
+     * @returns URL to query Translink web service for arrival data
+     */
+    protected URL getURL() throws MalformedURLException {
+        if (stop == null)throw new MalformedURLException();
+
+        String urlString = "http://api.translink.ca/rttiapi/v1/stops/"+stop.getNumber()+"/estimates?apikey=AUTdIc0W3SywrqGn6OID";
+        URL url = new URL(urlString);
+
+
+        return url;
+    }
+
+    @Override
+    public byte[] dataSourceToBytes() throws IOException {
+        return new byte[0];
+    }
+}
---
+++ buses are us/auth/TranslinkToken.java
@@ -0,0 +1,5 @@
+package ca.ubc.cs.cpsc210.translink.auth;
+
+public class TranslinkToken {
+    public static final String TRANSLINK_API_KEY = "AUTdIc0W3SywrqGn6OID";
+}
